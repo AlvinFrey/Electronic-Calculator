ac30ba538f63a4aed9ab999c5f63e11cd1aeb9ea
Tixis <Contact@tixis.org>
2016-01-25 18:03:12 +0100

    Added new files

--- FR/app/js/main.js
+++ FR/app/js/main.js
@@ -1,10 +1,6 @@
 var nw = require('nw.gui');
 var win = nw.Window.get();
 
-document.getElementById('refresh').onclick = function(){
-    win.reload();
-};
-
 document.getElementById('minimize').onclick = function(){
     win.minimize();
 };
